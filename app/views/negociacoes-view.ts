4cee3e552aad1b3d4c0e46741598664a477e7d13
jorgehaf <jorgehenriqueaf@gmail.com>
2022-06-24 15:53:21 -0300

    Avançado no uso de Generics

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,12 +1,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
-
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
+export class NegociacoesView extends View<Negociacoes>{
 
     template(model: Negociacoes): string {
         return `
@@ -31,10 +26,5 @@ export class NegociacoesView {
             </tbody>
         </table>
         `;
-    }
-
-    update(model: Negociacoes): void {
-        let template = this.template(model);
-        this.elemento.innerHTML = template;
     }
 }
